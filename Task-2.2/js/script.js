75f1126487673f57999eea478fdbaa4cbe8732b0
bzsky1 <bohdan.zahlynskyi@gmail.com>
2023-02-04 00:05:27 +0100

    Complited Task-2.2

--- Task-2.2/js/script.js
+++ Task-2.2/js/script.js
@@ -13,40 +13,42 @@ const allDesserts = document.querySelectorAll('#dessert')
 
 
 
-
-
+function unPack (arr) {
+    let res = ''
+    for (let i = 0; i < arr.length; i++) {
+        res += arr[i].innerHTML
+    }
+    return res
+}
 
 
 
 
 all.onclick = () => {
-    content.innerHTML = allItems[0].innerHTML + allItems[1].innerHTML + allItems[2].innerHTML + allItems[3].innerHTML + allItems[4].innerHTML + allItems[5].innerHTML + allItems[6].innerHTML + allItems[7].innerHTML + allItems[8].innerHTML + allItems[9].innerHTML + allItems[10].innerHTML + allItems[11].innerHTML + allItems[12].innerHTML
+    content.style.rowGap = '10vh'
+    content.innerHTML = unPack(allItems)
 }
 
 
 salads.onclick = () => {
-    content.style
-    .rowGap = '10vh'
-    content.innerHTML = allSalads[0].innerHTML + allSalads[1].innerHTML + allSalads[2].innerHTML
+    content.style.rowGap = '10vh'
+    content.innerHTML = unPack(allSalads)
 }
 
 
 soups.onclick = () => {
-    content.style
-    .rowGap = '10vh'
-    content.innerHTML = allSopus[0].innerHTML + allSopus[1].innerHTML + allSopus[2].innerHTML + allSopus[3].innerHTML
+    content.style.rowGap = '10vh'
+    content.innerHTML = unPack(allSopus)
 }
 
 
 khachapuris.onclick = () => {
-    content.style
-    .rowGap = '10vh'
-    content.innerHTML = allKhachapuri[0].innerHTML + allKhachapuri[1].innerHTML
+    content.style.rowGap = '10vh'
+    content.innerHTML = unPack(allKhachapuri)
 }
 
 
 desserts.onclick = () => {
-    content.style
-    .rowGap = '10vh'
-    content.innerHTML = allDesserts[0].innerHTML + allDesserts[1].innerHTML + allDesserts[2].innerHTML + allDesserts[3].innerHTML
+    content.style.rowGap = '10vh'
+    content.innerHTML = unPack(allDesserts)
 }
