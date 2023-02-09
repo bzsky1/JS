483231968bd7731d44cb908090cd556ea3c5db13
bzsky1 <bohdan.zahlynskyi@gmail.com>
2023-02-09 23:09:28 +0100

    Complited Task-2.2

--- Task-2.2/js/script.js
+++ Task-2.2/js/script.js
@@ -1,15 +1,10 @@
 const all = document.querySelector('#all')
 const salads = document.querySelector('#salads')
 const soups = document.querySelector('#soups')
-const khachapuris = document.querySelector('#khachapuris')
+const khachapuri = document.querySelector('#khachapuri')
 const desserts = document.querySelector('#desserts')
 const content = document.querySelector('.content')
-
 const allItems = document.querySelectorAll('.item')
-// const allSalads = document.querySelectorAll('#salad')
-// const allSopus = document.querySelectorAll('#soup')
-// const allKhachapuri = document.querySelectorAll('#khachapuri')
-// const allDesserts = document.querySelectorAll('#dessert')
 
 
 all.onclick = () => {
@@ -25,10 +20,10 @@ all.onclick = () => {
 salads.onclick = () => {
     content.style.rowGap = '10.4vh'
     allItems.forEach(function(item) {
-        if (item.dataset.cat !== 'salad') {
-            item.style.display = 'none'
-        } else if (item.dataset.cat === 'salad') {
+        if (item.dataset.cat === 'salads') {
             item.style.display = 'block'
+        } else if (item.dataset.cat !== 'salad') {
+            item.style.display = 'none'
         }
     })
 }
@@ -37,22 +32,22 @@ salads.onclick = () => {
 soups.onclick = () => {
     content.style.rowGap = '10.4vh'
     allItems.forEach(function(item) {
-        if (item.dataset.cat !== 'soup') {
-            item.style.display = 'none'
-        } else if (item.dataset.cat === 'soup') {
+        if (item.dataset.cat === 'soups') {
             item.style.display = 'block'
+        } else if (item.dataset.cat !== 'soups') {
+            item.style.display = 'none'
         }
     })
 }
 
 
-khachapuris.onclick = () => {
+khachapuri.onclick = () => {
     content.style.rowGap = '10.4vh'
     allItems.forEach(function(item) {
-        if (item.dataset.cat !== 'khachapuri') {
-            item.style.display = 'none'
-        } else if (item.dataset.cat === 'khachapuri') {
+        if (item.dataset.cat === 'khachapuri') {
             item.style.display = 'block'
+        } else if (item.dataset.cat !== 'khachapuri') {
+            item.style.display = 'none'
         }
     })
 }
@@ -61,49 +56,10 @@ khachapuris.onclick = () => {
 desserts.onclick = () => {
     content.style.rowGap = '10.4vh'
     allItems.forEach(function(item) {
-        if (item.dataset.cat !== 'dessert') {
-            item.style.display = 'none'
-        } else if (item.dataset.cat === 'dessert') {
+        if (item.dataset.cat === 'desserts') {
             item.style.display = 'block'
+        } else if (item.dataset.cat !== 'desserts') {
+            item.style.display = 'none'
         }
     })
 }
-
-
-// function unPack (arr) {
-//     let res = ''
-//     for (let i = 0; i < arr.length; i++) {
-//         res += arr[i].innerHTML
-//     }
-//     return res
-// }
-
-
-// all.onclick = () => {
-//     content.style.rowGap = '10vh'
-//     content.innerHTML = unPack(allItems)
-// }
-
-
-// salads.onclick = () => {
-//     content.style.rowGap = '10vh'
-//     content.innerHTML = unPack(allSalads)
-// }
-
-
-// soups.onclick = () => {
-//     content.style.rowGap = '10vh'
-//     content.innerHTML = unPack(allSopus)
-// }
-
-
-// khachapuris.onclick = () => {
-//     content.style.rowGap = '10vh'
-//     content.innerHTML = unPack(allKhachapuri)
-// }
-
-
-// desserts.onclick = () => {
-//     content.style.rowGap = '10vh'
-//     content.innerHTML = unPack(allDesserts)
-// }
