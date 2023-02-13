471ccc6cd8dcea0d4dd459363fb83f49f5e701af
bzsky1 <bohdan.zahlynskyi@gmail.com>
2023-02-13 18:59:03 +0100

    Complited Task-2.2

--- Task-2.2/js/script.js
+++ Task-2.2/js/script.js
@@ -5,10 +5,18 @@ const khachapuri = document.querySelector('#khachapuri')
 const desserts = document.querySelector('#desserts')
 const content = document.querySelector('.content')
 const allItems = document.querySelectorAll('.item')
+const links = document.querySelectorAll('.header__link')
 
 
 all.onclick = () => {
     content.style.rowGap = '10.4vh'
+    links.forEach(function(link) {
+        if (link.dataset.link === 'all') {
+            link.classList.add('active')
+        } else if (link.dataset.link !== 'all') {
+            link.classList.remove('active')
+        }
+    })
     allItems.forEach(function(item) {
         if (item.dataset.cat !== '0') {
             item.style.display = 'block'
@@ -22,6 +30,13 @@ salads.onclick = () => {
     allItems.forEach(function(item) {
         if (item.dataset.cat === 'salads') {
             item.style.display = 'block'
+            links.forEach(function(link) {
+                if (link.dataset.link === 'salads') {
+                    link.classList.add('active')
+                } else if (link.dataset.link !== 'salads') {
+                    link.classList.remove('active')
+                }
+            })
         } else if (item.dataset.cat !== 'salad') {
             item.style.display = 'none'
         }
@@ -34,6 +49,13 @@ soups.onclick = () => {
     allItems.forEach(function(item) {
         if (item.dataset.cat === 'soups') {
             item.style.display = 'block'
+            links.forEach(function(link) {
+                if (link.dataset.link === 'soups') {
+                    link.classList.add('active')
+                } else if (link.dataset.link !== 'soups') {
+                    link.classList.remove('active')
+                }
+            })
         } else if (item.dataset.cat !== 'soups') {
             item.style.display = 'none'
         }
@@ -46,6 +68,13 @@ khachapuri.onclick = () => {
     allItems.forEach(function(item) {
         if (item.dataset.cat === 'khachapuri') {
             item.style.display = 'block'
+            links.forEach(function(link) {
+                if (link.dataset.link === 'khachapuri') {
+                    link.classList.add('active')
+                } else if (link.dataset.link !== 'khachapuri') {
+                    link.classList.remove('active')
+                }
+            })
         } else if (item.dataset.cat !== 'khachapuri') {
             item.style.display = 'none'
         }
@@ -58,6 +87,13 @@ desserts.onclick = () => {
     allItems.forEach(function(item) {
         if (item.dataset.cat === 'desserts') {
             item.style.display = 'block'
+            links.forEach(function(link) {
+                if (link.dataset.link === 'desserts') {
+                    link.classList.add('active')
+                } else if (link.dataset.link !== 'desserts') {
+                    link.classList.remove('active')
+                }
+            })
         } else if (item.dataset.cat !== 'desserts') {
             item.style.display = 'none'
         }
