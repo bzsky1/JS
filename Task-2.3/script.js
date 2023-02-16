4cbdf6924ca612409ce5199be2c5fee0c064490f
bzsky1 <bohdan.zahlynskyi@gmail.com>
2023-02-16 12:06:35 +0100

    Complited Task-2.3

--- Task-2.3/script.js
+++ Task-2.3/script.js
@@ -3,44 +3,20 @@ const result = document.querySelector('.result')
 const button = document.querySelector('.button')
 
 
-
 const ar = ['all', 'salads', 'soups', 'khachapuri', 'desserts', 'sushi', 'drinks', 'hot', 'grill', 'special', 'dip', 'sauce']
-
-
-function s (ar) {
-    for (let j = 0; j < ar.length; j++) {
-        for (let i = 0; i < ar.length - 1; i++) {
-            if (ar[i].length > ar[i+1].length) {
-                let temp = ar[i]
-                ar[i] = ar[i+1]
-                ar[i+1] = temp
-            } else if (ar[i].length === ar[i+1].length) {
-                if (ar[i] > ar[i+1]) {
-                    let temp = ar[i]
-                    ar[i] = ar[i+1]
-                    ar[i+1] = temp
-                }
-            }
-        }
-    }
-    return ar
-}
-
-
 
 
 sample.innerHTML = `Unsorted array: <br> ${ar}`
 
-button.onclick = () => {
 
-    result.innerHTML = s(ar)
+button.onclick = () => {
+    result.innerHTML = ar.sort((a, b) => {
+        if (a.length === b.length) {
+            return a.localeCompare(b)
+        } else {
+            return a.length - b.length
+        }
+    })
 }
 
 
-// result.innerHTML = ar.sort((a, b) => {
-//     if (a.length === b.length) {
-//         return a.localeCompare(b)
-//     } else {
-//         return a.length - b.length
-//     }
-// })
